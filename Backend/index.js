cad0fe928a63621faaabc69acbe7684700f15436
Abiral Jain <abiral2724@gmail.com>
2025-01-28 23:29:42 +0530

    error3

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,17 @@ const cors = require("cors");
 const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
-require('dotenv').config();
+
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
+const corsOptions = {
+    origin:'https://youtube-video-downloader-1-aako.onrender.com/',
+    credential:true
+}
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5003;
 
