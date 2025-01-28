c17158ca546611d4c991ef774fad228a93c38813
Abiral Jain <abiral2724@gmail.com>
2025-01-28 23:36:57 +0530

    error 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const YouTubeDownloader = () => {
     setIsLoading(true);
 
     try {
-      const response = await axios.post("https://youtube-video-downloader-df60.onrender.com/download", { url }, {
+      const response = await axios.post("https://youtube-video-downloader-2-8u9x.onrender.com/download", { url }, {
         responseType: "blob",
       });
 
